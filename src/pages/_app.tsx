bbf6ed4d729908478b5825f666c19c360aaa567f
Jonas Keinholz <git@inyono.dev>
2022-04-20 20:08:02 +0200

    fix types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,6 @@ import type { AppProps } from 'next/app'
 import '../styles.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  // @ts-expect-error
   return <Component {...pageProps} />
 }
