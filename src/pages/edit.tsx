23ae7d5bf1a75fc8cafc2dccc93aac2f1e6501f3
Jonas Keinholz <git@inyono.dev>
2022-04-21 09:50:40 +0200

    edit: undo, redo, save

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -1,10 +1,26 @@
-import { Editor } from '@edtr-io/core'
+import {
+  Editor,
+  useScopedDispatch,
+  useScopedSelector,
+  useScopedStore,
+} from '@edtr-io/core'
+import classNames from 'classnames'
 import { GetServerSideProps } from 'next'
+import { ReactNode } from 'react'
 
 import { Layout } from '../layout'
 import { plugins } from '../plugins'
 import { getJsonBody } from '../utils/get-json-body'
 import { kitchenSink } from '../fixtures/kitchen-sink'
+import {
+  hasRedoActions,
+  hasUndoActions,
+  hasPendingChanges as hasPendingChangesSelector,
+  undo,
+  redo,
+  serializeRootDocument,
+  persist,
+} from '@edtr-io/store'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   if (context.req.method !== 'POST') {
@@ -12,6 +28,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       // TODO: revert this
       props: {
         state: kitchenSink,
+        saveUrl: '#',
+        savePayload: {
+          foo: 'bar',
+        },
       },
     }
   }
@@ -32,11 +52,98 @@ export interface EditProps {
 
 export default function Edit(props: EditProps) {
   return (
-    <Layout>
+    <>
       <Editor
         plugins={plugins}
         initialState={props.state ?? { plugin: 'rows' }}
-      />
-    </Layout>
+      >
+        {(document) => {
+          return <EditInner {...props}>{document}</EditInner>
+        }}
+      </Editor>
+    </>
+  )
+}
+
+function EditInner({
+  children,
+  saveUrl,
+  savePayload,
+}: { children: ReactNode } & EditProps) {
+  const dispatch = useScopedDispatch()
+  const store = useScopedStore()
+  const undoable = useScopedSelector(hasUndoActions())
+  const redoable = useScopedSelector(hasRedoActions())
+  const hasPendingChanges = useScopedSelector(hasPendingChangesSelector())
+
+  return (
+    <>
+      <div className="bg-indigo-600">
+        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between flex-wrap">
+            <div className="w-0 flex-1 flex items-center" />
+            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+              <button
+                type="button"
+                className={classNames(
+                  'flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50',
+                  {
+                    'opacity-50 cursor-not-allowed': !undoable,
+                  }
+                )}
+                disabled={!undoable}
+                onClick={() => {
+                  dispatch(undo())
+                }}
+              >
+                Undo
+              </button>
+            </div>
+            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto sm:ml-3">
+              <button
+                type="button"
+                className={classNames(
+                  'flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50',
+                  {
+                    'opacity-50 cursor-not-allowed': !redoable,
+                  }
+                )}
+                disabled={!redoable}
+                onClick={() => {
+                  dispatch(redo())
+                }}
+              >
+                Redo
+              </button>
+            </div>
+            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto sm:ml-3">
+              <button
+                type="button"
+                className={classNames(
+                  'flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50',
+                  {
+                    'opacity-50 cursor-not-allowed': !hasPendingChanges,
+                  }
+                )}
+                disabled={!hasPendingChanges}
+                onClick={async () => {
+                  await fetch(saveUrl, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                      state: serializeRootDocument()(store.getState()),
+                      payload: savePayload,
+                    }),
+                  })
+                  dispatch(persist())
+                }}
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Layout>{children}</Layout>
+    </>
   )
 }
