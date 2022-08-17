90db16345d5b100efd32b3553e3ab9465c553cc4
Jonas Keinholz <git@inyono.dev>
2022-08-17 11:16:24 +0200

    fix build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,5 @@ import type { AppProps } from 'next/app'
 import '../styles.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  // @ts-expect-error
   return <Component {...pageProps} />
 }
