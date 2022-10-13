fa5df3ffe5a5130bfe0e93120406a64d6c56b4ae
Stephan Kulla <git.mail@kulla.me>
2022-10-13 14:08:29 +0200

    feat: Show asset after deeplink flow

--- src/pages/platform/index.tsx
+++ src/pages/platform/index.tsx
@@ -21,7 +21,7 @@ export default function Platform() {
     'http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/lti/v13/lti13'
   )
   url.searchParams.append('login_hint', 'editor')
-  url.searchParams.append('lti_message_hint', 'editor')
+  url.searchParams.append('lti_message_hint', 'deep-link')
   url.searchParams.append('client_id', 'editor')
   url.searchParams.append('lti_deployment_id', '2')
 
@@ -56,7 +56,25 @@ export default function Platform() {
   })*/
 
   if (resourceLink) {
-    return <pre>{JSON.stringify(resourceLink, null, 2)}</pre>
+    const url = new URL(
+      'http://localhost:3001/edu-sharing/rest/lti/v13/oidc/login_initiations'
+    )
+
+    url.searchParams.append('iss', 'http://localhost:3000')
+    url.searchParams.append('target_link_uri', resourceLink['url'])
+    url.searchParams.append('login_hint', 'editor')
+    url.searchParams.append('lti_message_hint', 'resource-link')
+    url.searchParams.append('client_id', 'editor')
+    url.searchParams.append('lti_deployment_id', '2')
+
+    return (
+      <>
+        <h1>State</h1>
+        <pre>{JSON.stringify(resourceLink, null, 2)}</pre>
+        <h2>Eingebundene Ressource</h2>
+        <iframe src={url.href} style={{ width: '100%', height: '50vh' }} />
+      </>
+    )
   }
 
   if (picking) {
