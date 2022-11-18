19395e949277c21db7602f6ae3c06ff1372c8cd4
ortwic <ohc84@gmx.de>
2022-11-18 10:33:00 +0100

    lint prettier

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -2,35 +2,35 @@ import fetch from 'node-fetch'
 
 type KeyResponse = { keys: Array<IKey> }
 interface IKey {
-    kid: string;
-    alg: 'RS256';
-    use: 'sig';
-    kty: 'RSA';
-    n: string;
-    e: string;
+  kid: string
+  alg: 'RS256'
+  use: 'sig'
+  kty: 'RSA'
+  n: string
+  e: string
 }
 
 describe('Calls for LTI request tests', () => {
-    const port = parseInt(process.env.PORT, 10) || 3000
-    const url = `http://localhost:${port}`
+  const port = parseInt(process.env.PORT, 10) || 3000
+  const url = `http://localhost:${port}`
 
-    test('/platform/keys', async () => {
-        const response = await fetch(`${url}/platform/keys`)
-        const json: KeyResponse = await response.json()
-        
-        expect(response.status).toBe(200)
-        expect(json.keys.length).toBeGreaterThan(0)
-        expect(json.keys[0].kid).toEqual('42')
-        expect(json.keys[0].n.length).toBeGreaterThan(0)
-    })
+  test('/platform/keys', async () => {
+    const response = await fetch(`${url}/platform/keys`)
+    const json: KeyResponse = await response.json()
 
-    test('/lti/keys', async () => {
-        const response = await fetch(`${url}/lti/keys`)
-        const json: KeyResponse = await response.json()
+    expect(response.status).toBe(200)
+    expect(json.keys.length).toBeGreaterThan(0)
+    expect(json.keys[0].kid).toEqual('42')
+    expect(json.keys[0].n.length).toBeGreaterThan(0)
+  })
 
-        expect(response.status).toBe(200)
-        expect(json.keys.length).toBeGreaterThan(0)
-        expect(json.keys[0].n.length).toBeGreaterThan(0)
-        expect(json.keys[1].n.length).toBeGreaterThan(0)
-    })
+  test('/lti/keys', async () => {
+    const response = await fetch(`${url}/lti/keys`)
+    const json: KeyResponse = await response.json()
+
+    expect(response.status).toBe(200)
+    expect(json.keys.length).toBeGreaterThan(0)
+    expect(json.keys[0].n.length).toBeGreaterThan(0)
+    expect(json.keys[1].n.length).toBeGreaterThan(0)
+  })
 })
--- __tests__/editor.test.ts
+++ __tests__/editor.test.ts
@@ -1,30 +1,18 @@
 import fetch from 'node-fetch'
 
 describe.skip('Editor tests', () => {
-    const port = parseInt(process.env.PORT, 10) || 3000
-    const url = `http://localhost:${port}`
+  const port = parseInt(process.env.PORT, 10) || 3000
+  const url = `http://localhost:${port}`
 
-    test('Open editor with LTI', () => {
-        
-    })
+  test('Open editor with LTI', () => {})
 
-    test('Open editor in view mode when postContentApiUrl is not present ', () => {
-        
-    })
+  test('Open editor in view mode when postContentApiUrl is not present ', () => {})
 
-    test('Button "Close and Save"', () => {
-        
-    })
+  test('Button "Close and Save"', () => {})
 
-    test('Button "name version"', () => {
-        
-    })
+  test('Button "name version"', () => {})
 
-    test('Content is saved after 5 secs', () => {
-        
-    })
+  test('Content is saved after 5 secs', () => {})
 
-    test('Content is saved when closed with a tab', () => {
-        
-    })
+  test('Content is saved when closed with a tab', () => {})
 })
