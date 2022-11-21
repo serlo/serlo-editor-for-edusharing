b2325362f0432ebeccd711b536733bada171f25a
Stephan Kulla <git.mail@kulla.me>
2022-11-21 15:07:56 +0100

    Improvements for the tests (#46)
    * Update jest.config.ts
    
    * Refactor __tests__/api.test.ts
    
    * consider aaa pattern
    
    Co-authored-by: ortwic <ohc84@gmx.de>

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,35 +1,55 @@
+import { loadEnvConfig } from '@next/env'
 import fetch from 'node-fetch'
 
-type KeyResponse = { keys: Array<IKey> }
-interface IKey {
-  kid: string
-  alg: 'RS256'
-  use: 'sig'
-  kty: 'RSA'
-  n: string
-  e: string
-}
-
 describe('Calls for LTI request tests', () => {
-  const port = parseInt(process.env.PORT, 10) || 3000
-  const url = `http://localhost:${port}`
+  loadEnvConfig(process.cwd())
+  const url = process.env.EDITOR_URL
 
   test('/platform/keys', async () => {
-    const response = await fetch(`${url}/platform/keys`)
-    const json: KeyResponse = await response.json()
+    // Arrange
+    const expected = {
+      keys: [
+        {
+          kid: '42',
+          alg: 'RS256',
+          use: 'sig',
+          kty: 'RSA',
+          n: '4sz6WSrEX2wP_Q1I8dR1Ng4OvlCsi1-iylDt6-QVGhEKkg4uUzBCuTPfKOOPfZ418H1y2vNxS3P8wpS7OkdtOW5x8p5HcmELaGxpPVO0aqNYVcbVLIIzKvj9r9QCixg44vkvmfGj5mT6JJYWJOLMOIXILlzKT2QhpIamxbGsufQQ36ut5pxmC93-AimAbRz9h30xOCIZtUy5SUVKnKJKx5_crJEDLIOan5CsVFGbqZ59B_E_KT8ikwT804zpiEa22R__zVH27fX-RuHobYXBCQMpSO8QwEDIbSNPJIGOLRC_WeQ5U8-uY4Tx7Rj5n9wrk5kE5iZDW3O1swGAwGM1tw',
+          e: 'AQAB',
+        },
+      ],
+    }
 
+    // Act
+    const response = await fetch(`${url}/platform/keys`)
+    const result = await response.json()
+
+    // Assert
     expect(response.status).toBe(200)
-    expect(json.keys.length).toBeGreaterThan(0)
-    expect(json.keys[0].kid).toEqual('42')
-    expect(json.keys[0].n).toBeDefined()
+    expect(result).toEqual(expected)
   })
 
   test('/lti/keys', async () => {
-    const response = await fetch(`${url}/lti/keys`)
-    const json: KeyResponse = await response.json()
+    // Arrange
+    const expected = {
+      keys: [
+        {
+          kty: 'RSA',
+          n: expect.any(String),
+          e: 'AQAB',
+          kid: expect.any(String),
+          alg: 'RS256',
+          use: 'sig',
+        },
+      ],
+    }
 
+    // Act
+    const response = await fetch(`${url}/lti/keys`)
+    const result = await response.json()
+
+    // Assert
     expect(response.status).toBe(200)
-    expect(json.keys.length).toBeGreaterThan(0)
-    expect(json.keys[0].n).toBeDefined()
+    expect(result).toEqual(expected)
   })
 })
