fddc9d06231f14a9ec5308cd40aa4f3537787826
Stephan Kulla <git.mail@kulla.me>
2022-12-05 20:29:45 +0100

    edusharing-mock: Add mock for auth link

--- edusharing-server.ts
+++ edusharing-server.ts
@@ -38,6 +38,12 @@ app.get('/', (_req, res) => {
   `)
 })
 
+app.get('/edu-sharing/rest/ltiplatform/v13/auth', (req, res) => {
+  // http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/ltiplatform/v13/auth?response_type=id_token&response_mode=form_post&id_token_signed_response_alg=RS256&scope=openid&client_id=qsa2DgKBJ2WgoJO&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flti&login_hint=admin&nonce=43pesgc8i6ptw83xpvwaa00tc&prompt=none&state=5e867c7aae19b74e9a3815ea38b2c8f848f91c6b2c4468d890&lti_message_hint=d882efaa-1f84-4a0f-9bc9-4f74f19f7576&lti_deployment_id=1
+
+  res.send('Nonce: ' + req.query['nonce'])
+})
+
 app.listen(edusharingPort, () => {
   console.log('INFO: Mocked version of edusharing is ready.')
   console.log(
