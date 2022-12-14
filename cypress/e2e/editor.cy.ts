d59afa4d9cc92198f3a7e208ff064971bfe06b03
Stephan Kulla <git.mail@kulla.me>
2022-12-14 17:04:29 +0100

    cypress: Test saving of version

--- cypress/e2e/editor.cy.ts
+++ cypress/e2e/editor.cy.ts
@@ -3,4 +3,21 @@ it('The editor can be called via the LTI Workflow', () => {
   cy.contains('Speichern & Schließen')
 })
 
+it.only('Button "Saved named version" saves a named version', () => {
+  cy.request('DELETE', 'http://localhost:8100/_internals/saved-versions')
+
+  cy.visit('http://localhost:8100')
+  cy.contains('Benannte Version speichern').click()
+  cy.get('input[placeholder="Name der neuen Version"]').type('version-name')
+  cy.contains(/^Speichern$/).click()
+
+  cy.request('http://localhost:8100/_internals/saved-versions').then(
+    (response) => {
+      expect(response.body)
+        .to.be.an('array')
+        .that.deep.includes({ comment: 'version-name' })
+    }
+  )
+})
+
 export {}
