b2f5fba2e1e2f9a6cc9f1d812e4825e2b8fe7dad
Stephan Kulla <git.mail@kulla.me>
2022-12-14 16:15:22 +0100

    cypress: Set "chromeWebSecurity" to "false"

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  // Disable checks in chrome "same-origin" in HTTP requests. Normally cypress
+  // implements measures to avoid this problem. However due to the LTI the
+  // domain changes often in the LTI worflow.
+  //
+  // see https://docs.cypress.io/guides/guides/web-security for more details
+  //
+  // TODO: Find a way to avoid this setting
+  chromeWebSecurity: false,
 })
