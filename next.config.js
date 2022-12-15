197199e63e3636c38aae459b82467f89bd3f23ec
Botho <1258870+elbotho@users.noreply.github.com>
2022-12-15 17:04:56 +0100

    remove typehelper

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  resolve: {
-    alias: {
-      // fixes problem with outdated react-dnd version
-      // see https://github.com/react-dnd/react-dnd/issues/3433
-      // can be removed if edtr is on react-dnd 16
-      'react/jsx-runtime.js': 'react/jsx-runtime',
-      'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
-    },
+  webpack(config) {
+    // fixes problem with outdated react-dnd version
+    // see https://github.com/react-dnd/react-dnd/issues/3433
+    // can be removed if edtr is on react-dnd 16
+    config.resolve.alias['react/jsx-runtime.js'] = 'react/jsx-runtime'
+    config.resolve.alias['react/jsx-dev-runtime.js'] = 'react/jsx-dev-runtime'
+
+    return config
   },
 }
 
