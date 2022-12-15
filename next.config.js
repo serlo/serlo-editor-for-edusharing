6d5e944e7bdc1d3481fd1217529c0a5f55511db8
Botho <1258870+elbotho@users.noreply.github.com>
2022-12-15 16:49:22 +0100

    update types

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  resolve: {
+    alias: {
+      // fixes problem with outdated react-dnd version
+      // see https://github.com/react-dnd/react-dnd/issues/3433
+      // can be removed if edtr is on react-dnd 16
+      'react/jsx-runtime.js': 'react/jsx-runtime',
+      'react/jsx-dev-runtime.js': 'react/jsx-dev-runtime',
+    },
+  },
 }
 
 module.exports = nextConfig
