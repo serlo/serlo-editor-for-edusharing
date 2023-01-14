8a4ea81a3eff37a05b876a90d10a2f0d5f6a3bc8
Stephan Kulla <git.mail@kulla.me>
2023-01-14 17:55:15 +0100

    test: Fix sending of client_id

--- src/edusharing-server-mock.ts
+++ src/edusharing-server-mock.ts
@@ -165,7 +165,7 @@ export class EdusharingServer {
             state: this.state,
             lti_message_hint: messageHint,
             redirect_uri: process.env.EDITOR_TARGET_DEEP_LINK_URL,
-            client_id: process.env.PLATFORM_CLIENT_ID,
+            client_id: process.env.EDITOR_CLIENT_ID,
           },
         })
       }
