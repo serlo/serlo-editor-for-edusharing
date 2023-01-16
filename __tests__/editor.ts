1a2431a92b2f613e2d03aa9b2c2febec3bbfc5fa
Stephan Kulla <git.mail@kulla.me>
2023-01-16 09:04:26 +0100

    feat: /platform/done fails when message hint has invalid scheme

--- __tests__/editor.ts
+++ __tests__/editor.ts
@@ -52,6 +52,22 @@ describe('endpoint "/platform/login"', () => {
     expect(response.status).toBe(400)
     expect(await response.text()).toBe(`lti_message_hint is invalid`)
   })
+
+  test('fails when `lti_message_hint` has invalid scheme', async () => {
+    const url = new URL('http://localhost:3000/platform/login')
+
+    for (const [name, value] of Object.entries({
+      ...correctParamaters,
+      lti_message_hint: JSON.stringify({ foo: 'bar' }),
+    })) {
+      url.searchParams.append(name, value)
+    }
+
+    const response = await fetch(url.href)
+
+    expect(response.status).toBe(400)
+    expect(await response.text()).toBe(`lti_message_hint is invalid`)
+  })
 })
 
 describe('endpoint "/platform/done"', () => {
