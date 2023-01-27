1507577993d3a3767ac13a1b28cb1a4374085121
Stephan Kulla <git.mail@kulla.me>
2023-01-27 16:10:41 +0100

    feat: Add favicon
    Fixes https://github.com/serlo/ece-as-a-service/issues/168

--- src/frontend/serlo-editor.tsx
+++ src/frontend/serlo-editor.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import { Renderer } from '@edtr-io/renderer'
 
@@ -14,16 +15,21 @@ export interface SerloEditorProps extends EditorProps {
 }
 
 export function SerloEditor(props: SerloEditorProps) {
-  if (props.mayEdit) {
-    return <Editor {...props} />
-  } else {
-    return (
-      <Layout>
-        <Renderer
-          plugins={createPlugins({ ltik: props.ltik })}
-          state={props.state.document}
-        />
-      </Layout>
-    )
-  }
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+      </Head>
+      {props.mayEdit ? (
+        <Editor {...props} />
+      ) : (
+        <Layout>
+          <Renderer
+            plugins={createPlugins({ ltik: props.ltik })}
+            state={props.state.document}
+          />
+        </Layout>
+      )}
+    </>
+  )
 }
