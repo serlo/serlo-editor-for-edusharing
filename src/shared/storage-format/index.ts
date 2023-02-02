9e29fe7701e482ad67d03e12d977412372a8810b
Stephan Kulla <git.mail@kulla.me>
2023-02-02 20:51:25 +0100

    feat: Add variant to storage format
    Fixes #180
    
    With this feature we can detect in the future that the document was
    written by the code of this repository. So in case we need this
    information for migration we have stored it :-)

--- src/shared/storage-format/index.ts
+++ src/shared/storage-format/index.ts
@@ -1,17 +1,20 @@
 import { kitchenSink } from './kitchen-sink'
 
 export const documentType = 'https://github.com/serlo/ece-as-a-service'
+const variantType = 'https://github.com/serlo/serlo-editor-for-edusharing'
 const migrations: Migration[] = []
 const currentVersion = migrations.length
 
 export const emptyDocument: StorageFormat = {
   type: documentType,
+  variant: variantType,
   version: 0,
   document: { plugin: 'rows' },
 }
 
 export const kitchenSinkDocument: StorageFormat = {
   type: documentType,
+  variant: variantType,
   version: 0,
   document: kitchenSink,
 }
@@ -29,6 +32,7 @@ export function migrate(state: StorageFormat): StorageFormat {
 
   return {
     type: documentType,
+    variant: variantType,
     version: currentVersion,
     document,
   }
@@ -38,6 +42,7 @@ type Migration = (state: StorageFormat['document']) => StorageFormat['document']
 
 export interface StorageFormat {
   type: typeof documentType
+  variant: typeof variantType
   version: number
   document: { plugin: string; state?: unknown }
 }
