d4614c568c1dd4e7ccc15e7d937f2db68c971cb4
Lars Rasmussen <lars@lars-rasmussen.de>
2023-02-07 10:37:19 +0100

    Test: Add tests for requests to /lti

--- __tests__/editor-lti-endpoints.ts
+++ __tests__/editor-lti-endpoints.ts
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch'
-import { signJwtWithBase64Key } from '../src/server-utils'
+import { createJWKSResponse, signJwtWithBase64Key } from '../src/server-utils'
+import express from 'express'
+import { Server } from 'node:http'
 
 // See: https://cvmcosta.me/ltijs/#/provider?id=request-authentication
 describe('All requests to editor endpoints /lti/... shall return Unauthorized (401) if url parameter "ltik" is missing or invalid.', () => {
@@ -53,11 +55,124 @@ describe('endpoint "/lti/login"', () => {
   })
 })
 
+// REFACTOR There is duplicated code for starting an express server to provide an endpoint to retrieve the keyset for jwt signature validation. Here and in __tests__/editor.ts. Maybe setup a Jest test environment and a separate test file for all tests that need a working test double for edusharing listening on port 8100. This would reduce code duplication, make sure these tests run in sequence and would allow other jest test files to run in parallel meanwhile. See: https://jestjs.io/docs/configuration#testenvironment-string
 describe('endpoint "/lti"', () => {
-  let valideCookieValue: string
+  let validCookieValue: string
   let validState: string
+  const validKeyid = 'keyid'
+  const validPayloadInIdToken = {
+    nonce: 'nonce-value',
+    iss: process.env.PLATFORM_URL,
+    aud: process.env.PLATFORM_CLIENT_ID,
+    sub: 'admin',
+    'https://purl.imsglobal.org/spec/lti/claim/deployment_id': '1',
+    'https://purl.imsglobal.org/spec/lti/claim/context': {
+      id: 'd882efaa-1f84-4a0f-9bc9-4f74f19f7576',
+      label: 'Home des Unternehmens',
+    },
+    given_name: 'Administrator',
+    family_name: '',
+    email: 'admin@alfresco.com',
+    'https://purl.imsglobal.org/spec/lti/claim/tool_platform': {
+      name: 'local',
+      product_family_code: 'edu-sharing',
+      guid: 'serlo-edusharing',
+      description: 'local',
+      version: '9999.14076175.9999',
+    },
+    'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0',
+    'https://purl.imsglobal.org/spec/lti/claim/roles': [],
+    'https://purl.imsglobal.org/spec/lti/claim/target_link_uri':
+      'http://localhost:3000/lti',
+    'https://purl.imsglobal.org/spec/lti/claim/resource_link': {
+      id: '604f62c1-6463-4206-a571-8c57097a54ae',
+      title: 'Hello worldd',
+    },
+    'https://purl.imsglobal.org/spec/lti/claim/launch_presentation': {
+      document_target: 'window',
+      return_url:
+        'http://repository.127.0.0.1.nip.io:8100/edu-sharing/components/workspace?id=d882efaa-1f84-4a0f-9bc9-4f74f19f7576&mainnav=true&displayType=0',
+      locale: 'de_DE',
+    },
+    'https://purl.imsglobal.org/spec/lti/claim/message_type':
+      'LtiResourceLinkRequest',
+    'https://purl.imsglobal.org/spec/lti/claim/custom': {
+      getContentApiUrl:
+        'http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/ltiplatform/v13/content',
+      fileName: 'Hello worldd',
+      getDetailsSnippetUrl:
+        'http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/lti/v13/details',
+      dataToken:
+        'kOXGc6AbqYW7iHOl3b48Pj/ngudoLCZk+DJwYxAg9wTiKsN9TKRY13qU+6vNNMEV2Guya3NPWO+Ay8IJDtQWMKxnkku/3mc+n64TIgMjs2yY7wXMYcvoRK4C9iXXpydNWQCGreYU2BcnMwne/b5BngOvBjqqVCPLMGT/lmvylP//GCzM7V99h9fKVMrgY97qOdsB1O0Ti//E3odWU1dFUMu3NLPy3MdTHXdViQpyPFRpgnZ8kcywDl0bLYSKy0pUuJy0hBvlnGmFyKlcQ38HaR2CZ9wRxrNgRxxEzGd8J+T6YSNoD8OyB9Nyjbp0N3tog4XhEZ/UASIqLYBzk+ygOA==',
+      postContentApiUrl:
+        'http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/ltiplatform/v13/content',
+      appId: 'qsa2DgKBJ2WgoJO1',
+      nodeId: '604f62c1-6463-4206-a571-8c57097a54ae',
+      user: 'admin',
+    },
+  }
+  const defaultKeysetRequestHandler = (_req, res) => {
+    createJWKSResponse({
+      res,
+      keyid: validKeyid,
+      key: process.env.EDITOR_PLATFORM_PUBLIC_KEY,
+    })
+  }
+  let keysetRequestHandler = defaultKeysetRequestHandler
+  let server: Server
+
+  type RequestParametersToOverwrite = {
+    issuer?: string
+    idTokenExpireAfterSeconds?: number
+    nonce?: string
+    audience?: string
+  }
+  async function sendRequestToLtiEndpoint(params: {
+    overwriteParameters: RequestParametersToOverwrite
+  }) {
+    let payloadInIdToken = { ...validPayloadInIdToken }
+    if ('issuer' in params.overwriteParameters) {
+      payloadInIdToken.iss = params.overwriteParameters.issuer
+    }
+    if ('nonce' in params.overwriteParameters) {
+      payloadInIdToken.nonce = params.overwriteParameters.nonce
+    }
+    if ('audience' in params.overwriteParameters) {
+      payloadInIdToken.aud = params.overwriteParameters.audience
+    }
+
+    return fetch('http://localhost:3000/lti', {
+      method: 'POST',
+      headers: { Cookie: validCookieValue },
+      body: new URLSearchParams({
+        id_token: signJwtWithBase64Key({
+          payload: payloadInIdToken,
+          keyid: validKeyid,
+          key: process.env.EDITOR_PLATFORM_PRIVATE_KEY,
+          expireAfterSeconds:
+            'idTokenExpireAfterSeconds' in params.overwriteParameters
+              ? params.overwriteParameters.idTokenExpireAfterSeconds
+              : 60,
+        }),
+        state: validState,
+      }),
+      redirect: 'manual',
+    })
+  }
+
+  beforeAll((done) => {
+    const app = express()
+
+    app.get('/edu-sharing/rest/lti/v13/jwks', (req, res, next) => {
+      keysetRequestHandler(req, res)
+    })
+
+    server = app.listen(8100, done)
+  })
 
   beforeEach(async () => {
+    keysetRequestHandler = defaultKeysetRequestHandler
+
     const response = await fetch('http://localhost:3000/lti/login', {
       method: 'POST',
       body: new URLSearchParams({
@@ -71,80 +186,32 @@ describe('endpoint "/lti"', () => {
       redirect: 'manual',
     })
 
-    // Check that have a valid login request
+    // Check that we have a valid login request
     expect(response.status).toBe(302)
 
-    valideCookieValue = response.headers.get('set-cookie').split(';')[0]
+    validCookieValue = response.headers.get('set-cookie').split(';')[0]
     validState = new URL(response.headers.get('location')).searchParams.get(
       'state'
     )
   })
 
-  test('fails when wrong issuer is sent', async () => {
-    const payload = {
-      nonce: 'nonce-value',
-      iss: 'wrong-issuer',
-      aud: process.env.PLATFORM_CLIENT_ID,
-      sub: 'admin',
-      'https://purl.imsglobal.org/spec/lti/claim/deployment_id': '1',
-      'https://purl.imsglobal.org/spec/lti/claim/context': {
-        id: 'd882efaa-1f84-4a0f-9bc9-4f74f19f7576',
-        label: 'Home des Unternehmens',
-      },
-      given_name: 'Administrator',
-      family_name: '',
-      email: 'admin@alfresco.com',
-      'https://purl.imsglobal.org/spec/lti/claim/tool_platform': {
-        name: 'local',
-        product_family_code: 'edu-sharing',
-        guid: 'serlo-edusharing',
-        description: 'local',
-        version: '9999.14076175.9999',
-      },
-      'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0',
-      'https://purl.imsglobal.org/spec/lti/claim/roles': [],
-      'https://purl.imsglobal.org/spec/lti/claim/target_link_uri':
-        'http://localhost:3000/lti',
-      'https://purl.imsglobal.org/spec/lti/claim/resource_link': {
-        id: '604f62c1-6463-4206-a571-8c57097a54ae',
-        title: 'Hello worldd',
-      },
-      'https://purl.imsglobal.org/spec/lti/claim/launch_presentation': {
-        document_target: 'window',
-        return_url:
-          'http://repository.127.0.0.1.nip.io:8100/edu-sharing/components/workspace?id=d882efaa-1f84-4a0f-9bc9-4f74f19f7576&mainnav=true&displayType=0',
-        locale: 'de_DE',
-      },
-      'https://purl.imsglobal.org/spec/lti/claim/message_type':
-        'LtiResourceLinkRequest',
-      'https://purl.imsglobal.org/spec/lti/claim/custom': {
-        getContentApiUrl:
-          'http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/ltiplatform/v13/content',
-        fileName: 'Hello worldd',
-        getDetailsSnippetUrl:
-          'http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/lti/v13/details',
-        dataToken:
-          'kOXGc6AbqYW7iHOl3b48Pj/ngudoLCZk+DJwYxAg9wTiKsN9TKRY13qU+6vNNMEV2Guya3NPWO+Ay8IJDtQWMKxnkku/3mc+n64TIgMjs2yY7wXMYcvoRK4C9iXXpydNWQCGreYU2BcnMwne/b5BngOvBjqqVCPLMGT/lmvylP//GCzM7V99h9fKVMrgY97qOdsB1O0Ti//E3odWU1dFUMu3NLPy3MdTHXdViQpyPFRpgnZ8kcywDl0bLYSKy0pUuJy0hBvlnGmFyKlcQ38HaR2CZ9wRxrNgRxxEzGd8J+T6YSNoD8OyB9Nyjbp0N3tog4XhEZ/UASIqLYBzk+ygOA==',
-        postContentApiUrl:
-          'http://repository.127.0.0.1.nip.io:8100/edu-sharing/rest/ltiplatform/v13/content',
-        appId: 'qsa2DgKBJ2WgoJO1',
-        nodeId: '604f62c1-6463-4206-a571-8c57097a54ae',
-        user: 'admin',
-      },
-    }
+  afterAll((done) => {
+    server.close(done)
+  })
 
-    const response = await fetch('http://localhost:3000/lti', {
-      method: 'POST',
-      headers: { Cookie: valideCookieValue },
-      body: new URLSearchParams({
-        id_token: signJwtWithBase64Key({
-          payload,
-          keyid: 'key',
-          key: process.env.EDITOR_PLATFORM_PRIVATE_KEY,
-        }),
-        state: validState,
-      }),
-      redirect: 'manual',
+  test('succeeds', async () => {
+    const response = await sendRequestToLtiEndpoint({
+      overwriteParameters: {},
+    })
+
+    expect(response.status).toBe(302)
+  })
+
+  test('fails when wrong issuer is sent', async () => {
+    const response = await sendRequestToLtiEndpoint({
+      overwriteParameters: {
+        issuer: 'invalid-issuer',
+      },
     })
 
     expect(response.status).toBe(401)
@@ -154,6 +221,112 @@ describe('endpoint "/lti"', () => {
       details: {
         description: 'Error validating ltik or IdToken',
         message: 'ISS_CLAIM_DOES_NOT_MATCH',
+      },
+    })
+  })
+
+  test('fails when id_token expired', async () => {
+    const response = await sendRequestToLtiEndpoint({
+      overwriteParameters: {
+        idTokenExpireAfterSeconds: 0,
+      },
+    })
+
+    expect(response.status).toBe(401)
+    expect(await response.json()).toEqual({
+      status: 401,
+      error: 'Unauthorized',
+      details: {
+        description: 'Error validating ltik or IdToken',
+        message: 'jwt expired',
+      },
+    })
+  })
+
+  test('fails when no key id of requested public keys matches the one used to sign id_token', async () => {
+    keysetRequestHandler = (_req, res) => {
+      createJWKSResponse({
+        res,
+        keyid: 'invalid-key-id',
+        key: process.env.EDITOR_PLATFORM_PUBLIC_KEY,
+      })
+    }
+
+    const response = await sendRequestToLtiEndpoint({
+      overwriteParameters: {},
+    })
+
+    expect(response.status).toBe(401)
+    expect(await response.json()).toEqual({
+      status: 401,
+      error: 'Unauthorized',
+      details: {
+        description: 'Error validating ltik or IdToken',
+        message: 'KEY_NOT_FOUND',
+      },
+    })
+  })
+
+  test('fails when no public key could be retrieved', async () => {
+    keysetRequestHandler = (_req, res) => {
+      res.end()
+    }
+
+    const response = await sendRequestToLtiEndpoint({
+      overwriteParameters: {},
+    })
+
+    expect(response.status).toBe(401)
+    expect(await response.json()).toEqual({
+      status: 401,
+      error: 'Unauthorized',
+      details: {
+        description: 'Error validating ltik or IdToken',
+        message: 'KEYSET_NOT_FOUND',
+      },
+    })
+  })
+
+  test('fails when nonce in id_token does not match what was sent in login request', async () => {
+    keysetRequestHandler = (_req, res) => {
+      res.end()
+    }
+
+    const response = await sendRequestToLtiEndpoint({
+      overwriteParameters: {
+        nonce: 'invalid-nonce',
+      },
+    })
+
+    expect(response.status).toBe(401)
+    expect(await response.json()).toEqual({
+      status: 401,
+      error: 'Unauthorized',
+      details: {
+        description: 'Error validating ltik or IdToken',
+        message: 'KEYSET_NOT_FOUND',
+      },
+    })
+  })
+
+  test('fails when audience does not match the client id of lti tool', async () => {
+    keysetRequestHandler = (_req, res) => {
+      res.end()
+    }
+
+    const response = await sendRequestToLtiEndpoint({
+      overwriteParameters: {
+        audience: 'invalid-audience',
+      },
+    })
+
+    expect(response.status).toBe(401)
+    expect(await response.json()).toEqual({
+      status: 401,
+      error: 'Unauthorized',
+      details: {
+        description: 'Error validating ltik or IdToken',
+        message: 'UNREGISTERED_PLATFORM',
       },
     })
   })
