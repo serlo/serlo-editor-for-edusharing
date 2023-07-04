b663abb7a68ac983ef6fc926d8a5126db2573b97
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-07-04 16:59:58 +0200

    refactor(backend): try to fix typing

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -220,7 +220,7 @@ const server = (async () => {
     const request = new Request(url.href, {
       method: 'POST',
       headers: encoder.headers,
-      body: Readable.from(encoder.encode()),
+      body: JSON.stringify(encoder.encode()),
     })
 
     const response = await fetch(request)
