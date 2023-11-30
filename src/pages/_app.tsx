ec0490df377be1592dccc3b8cb6d2f7065103e64
Botho <1258870+elbotho@users.noreply.github.com>
2023-11-30 18:30:39 +0100

    mock styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 
-import '@serlo/editor/dist/editor-tailwind.css'
+// import '@serlo/editor/dist/editor-tailwind.css'
+import '../frontend/_tmp_styles.css'
 import 'katex/dist/katex.min.css'
 import '../frontend/styles.css'
 
