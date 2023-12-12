a3ad073383e77324f2f07f4158e8c9df7cbe34b7
Stephan Kulla <git.mail@kulla.me>
2023-12-12 18:51:03 +0100

    feat: Use createRenderers()

--- src/frontend/plugins/create-renderers.tsx
+++ src/frontend/plugins/create-renderers.tsx
@@ -3,8 +3,7 @@ import { ComponentProps } from 'react'
 
 import { Link } from '../link'
 import {
-  type InitRenderersArgs,
-  type LinkRenderer,
+  createRenderers as createBasicRenderers,
   BoxStaticRenderer,
   RowsStaticRenderer,
   // SpoilerStaticRenderer,
@@ -44,7 +43,9 @@ import { SerloInjectionStaticRenderer } from './serlo-injection/static'
 //     ),
 //   )
 
-export function createRenderers(): InitRenderersArgs {
+export function createRenderers(): ReturnType<typeof createBasicRenderers> {
+  const { pluginRenderers, ...otherRenderers } = createBasicRenderers()
+
   return {
     pluginRenderers: [
       // edu-sharing specific
@@ -53,100 +54,8 @@ export function createRenderers(): InitRenderersArgs {
         type: 'serloInjection',
         renderer: SerloInjectionStaticRenderer,
       },
-
-      // plugins
-      { type: EditorPluginType.Rows, renderer: RowsStaticRenderer },
-      { type: EditorPluginType.Text, renderer: TextStaticRenderer },
-      // {
-      //   type: EditorPluginType.Multimedia,
-      //   renderer: (state: EditorMultimediaDocument) => {
-      //     return <MultimediaStaticRenderer {...state} />
-      //   },
-      // },
-      // {
-      //   type: EditorPluginType.Spoiler,
-      //   renderer: (state: EditorSpoilerDocument) => {
-      //     return <SpoilerStaticRenderer {...state} />
-      //   },
-      // },
-      { type: EditorPluginType.Box, renderer: BoxStaticRenderer },
-      { type: EditorPluginType.SerloTable, renderer: SerloTableStaticRenderer },
-      // { type: EditorPluginType.Equations, renderer: EquationsStaticRenderer },
-      // Deactivated because geogebra embeds don't size themselves correctly in static renderer view. Having a relative div around GeogebraStaticRenderer already improved it (probably because of the absolute positioned child element). But now the geogebra applet has a height of 0, does not resize itself correctly to the space available. Specifying a height in the container div is not an option because applets have different heights.
-      // TODO: Fix issue with geogebra static renderer view and reenable
-      // {
-      //   type: EditorPluginType.Geogebra,
-      //   renderer: (props: EditorGeogebraDocument) => {
-      //     return (
-      //       <div className="relative">
-      //         <GeogebraStaticRenderer {...props} />
-      //       </div>
-      //     )
-      //   },
-      // },
-
-      // exercises
-      // {
-      //   type: EditorPluginType.Exercise,
-      //   renderer: (props: EditorExerciseDocument) => {
-      //     return <ExerciseStaticRenderer {...props} />
-      //   },
-      // },
-      // {
-      //   type: EditorPluginType.Highlight,
-      //   renderer: (props: EditorHighlightDocument) => {
-      //     return <HighlightStaticRenderer {...props} />
-      //   },
-      // },
-      // {
-      //   type: EditorPluginType.InputExercise,
-      //   renderer: (props: EditorInputExerciseDocument) => {
-      //     return <InputExerciseStaticRenderer {...props} />
-      //   },
-      // },
-      // {
-      //   type: EditorPluginType.ScMcExercise,
-      //   renderer: (props: EditorScMcExerciseDocument) => {
-      //     // @@@ Do these extra properties have to be mandatory?
-      //     return (
-      //       <ScMcExerciseStaticRenderer
-      //         {...props}
-      //         idBase="temp"
-      //         onEvaluate={() => {}}
-      //         renderExtraAnswerContent={() => <></>}
-      //       />
-      //     )
-      //   },
-      // },
-      // {
-      //   type: EditorPluginType.FillInTheBlanksExercise,
-      //   renderer: FillInTheBlanksStaticRenderer,
-      // },
-      // {
-      //   type: EditorPluginType.Solution,
-      //   renderer: (props: EditorSolutionDocument) => {
-      //     return (
-      //       <StaticSolutionRenderer {...props} solutionVisibleOnInit={false} />
-      //     )
-      //   },
-      // },
-
-      {
-        type: EditorPluginType.Unsupported,
-        renderer: (state: unknown) => {
-          // eslint-disable-next-line no-console
-          console.warn('unsupported renderer: ', state)
-          return null
-        },
-      },
+      ...pluginRenderers,
     ],
-    mathRenderer: (element: MathElement) => <StaticMath {...element} />,
-    linkRenderer: ({ href, children }: ComponentProps<LinkRenderer>) => {
-      return (
-        <>
-          <Link href={href}>{children}</Link>
-        </>
-      )
-    },
+    ...otherRenderers,
   }
 }
