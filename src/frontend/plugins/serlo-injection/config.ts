306570da25a7990ff0f9428e9be02fec0ba4f4f5
Lars Rasmussen <lars@lars-rasmussen.de>
2023-12-21 18:22:41 +0100

    fix: Make user enter serlo.org url instead of content id

--- src/frontend/plugins/serlo-injection/config.ts
+++ src/frontend/plugins/serlo-injection/config.ts
@@ -7,8 +7,9 @@ export function useSerloInjectionConfig(
 
   return {
     i18n: {
-      label: 'Serlo ID',
-      placeholder: '123456',
+      label:
+        'URL von einem serlo.org Inhalt (z.B. "https://de.serlo.org/mathe/1717/gleichung")',
+      placeholder: 'https://de.serlo.org/mathe/1717/gleichung',
       ...i18n,
     },
   }
