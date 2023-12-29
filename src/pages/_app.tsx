31c30f9e69e2d82da86fd712c5f9e48b58d45c29
Vitomir Budimir <budimirvitomir@gmail.com>
2023-12-29 13:52:44 +0100

    fix(editor): fix css import order

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from 'next/app'
 
+import '../frontend/tailwind-default-styles.css'
 import '@serlo/editor/dist/editor-tailwind.css'
 import 'katex/dist/katex.min.css'
-import '../frontend/styles.css'
+import '../frontend/tailwind-components-styles.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
