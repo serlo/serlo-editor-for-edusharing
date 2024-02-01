f6fba8431a2d5a5983c8ab78b271200373c683c6
Lars Rasmussen <lars@lars-rasmussen.de>
2024-02-01 11:43:39 +0100

    fix(text-links): Fix 'next router not mounted' error in static renderer view

--- src/frontend/plugins/create-renderers.tsx
+++ src/frontend/plugins/create-renderers.tsx
@@ -2,6 +2,9 @@ import { createRenderers as createBasicRenderers } from '@serlo/editor'
 
 import { EdusharingAssetStaticRenderer } from './edusharing-asset/static'
 import { SerloInjectionStaticRenderer } from './serlo-injection/static'
+import { ComponentProps } from 'react'
+import { LinkRenderer } from '@serlo/editor/dist/packages/editor/src/plugin/helpers/editor-renderer'
+import Link from 'next/link'
 
 export function createRenderers(): ReturnType<typeof createBasicRenderers> {
   const { pluginRenderers, ...otherRenderers } = createBasicRenderers()
@@ -17,5 +20,12 @@ export function createRenderers(): ReturnType<typeof createBasicRenderers> {
       ...pluginRenderers,
     ],
     ...otherRenderers,
+    linkRenderer: ({ href, children }: ComponentProps<LinkRenderer>) => {
+      return (
+        <>
+          <Link href={href}>{children}</Link>
+        </>
+      )
+    },
   }
 }
